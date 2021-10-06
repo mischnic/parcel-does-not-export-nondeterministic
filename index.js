227560f427777079eb1a129fa07691281dac32dc
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-10-06 21:33:35 +0200

    f

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-import * as Deck from "@deck.gl/core"
-console.log(Deck);
+// import * as Deck from "@deck.gl/core/dist/esm/utils/log"
+// // import * as Deck from "@deck.gl/core/dist/esm/lib/constants"
+// console.log(Deck);
+
+import { Log } from "probe.gl/dist/esm";
+console.log(Log);
+//# sourceMappingURL=log.js.map
